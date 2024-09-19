68690c8e6ec92e3d002784c8b4d3f2d0b53d3a7f
Loicarthur <89398456+Loicarthur@users.noreply.github.com>
2024-09-19 15:43:36 +0200

    Modifications locales dans app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./backoffice/page";
 import Customization from "./customization";
 // import PSVitaOLED from "./pages/PSVitaOLED";
@@ -11,20 +11,15 @@ import Customization from "./customization";
 import Accessories from "./accessories";
 import Wallpapers from "./wallpapers";
 
-const RootLayout = () => {
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <Router>
       <html lang="en">
         <body>
           <Header />
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/customization" element={< Customization/>} />
-            {/* <Route path="/psvita-oled" element={<PSVitaOLED />} />
-            <Route path="/editions-limitees" element={<EditionsLimitees />} /> */}
-            <Route path="/accessories" element={<Accessories />} />
-            <Route path="/wallpaper" element={<Wallpapers/>} />
-          </Routes>
+          {children}
           <Footer />
         </body>
       </html>
