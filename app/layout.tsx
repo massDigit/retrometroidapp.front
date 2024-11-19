bdc0f1a41907a9cf574be16de634892ac8fbd589
Loicarthur <89398456+Loicarthur@users.noreply.github.com>
2024-11-19 09:44:52 +0100

    Merge branch 'dev' into devv

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "./components/Footer";
+import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
